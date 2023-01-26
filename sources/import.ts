39430bcab108b8a592e2c5441f8a44726d46e271
Berend <berend@inuits.eu>
2023-01-26 10:51:13 +0100

    Add import source

---
+++ sources/import.ts
@@ -0,0 +1,26 @@
+import { AuthRESTDataSource } from 'inuits-apollo-server-auth';
+import { environment as env } from '../../../environment';
+import { extractParentDirectory } from '../parsers/directories';
+import { ImportReturn, Directory } from '../../../type-defs';
+
+export class ImportAPI extends AuthRESTDataSource {
+  public baseURL = `${env.api.csvImportServiceUrl}/`;
+
+  async getDirectories(dir: string): Promise<Directory[]> {
+    let data: Directory[] = [];
+
+    data = await this.get(`importer/directories?dir=${dir}`);
+
+    return extractParentDirectory(data);
+  }
+
+  async startImport(folder: string): Promise<ImportReturn> {
+    const data = await this.post(`importer/start`, {
+      body: {
+        selected_folder: folder,
+      },
+    });
+
+    return data;
+  }
+}
