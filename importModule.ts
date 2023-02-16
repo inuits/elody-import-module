d1f2441cfb82afe38b5a429f7927c7eebfa6ae23
Berend Vandenbussche <berend@inuits.eu>
2023-02-16 11:52:40 +0100

    Export ImportAPI

--- importModule.ts
+++ importModule.ts
@@ -1,12 +1,13 @@
-import { createModule } from 'graphql-modules';
-import { importSchema } from './importSchema.schema';
-import { importResolver } from './importResolver';
+import { createModule } from "graphql-modules";
+import { importSchema } from "./importSchema.schema";
+import { importResolver } from "./importResolver";
+import { ImportAPI } from "./sources/import";
 
 const importModule = createModule({
-  id: 'importModule',
+  id: "importModule",
   dirname: __dirname,
   typeDefs: [importSchema],
   resolvers: [importResolver],
 });
 
-export { importModule, importResolver, importSchema };
+export { importModule, importResolver, importSchema, ImportAPI };
