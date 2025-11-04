3eda69855b09de97c1bcaf29014e931d6522e02a
Berend Vandenbussche <berend@inuits.eu>
2025-11-04 14:36:12 +0100

    Chore: Restructure graphql environment startup

--- sources/import.ts
+++ sources/import.ts
@@ -1,9 +1,9 @@
-import { environment as env, AuthRESTDataSource } from "base-graphql";
+import { AuthRESTDataSource } from "base-graphql";
 import { extractParentDirectory } from "../parsers/directories";
 import { ImportReturn, Directory } from "../../../generated-types/type-defs";
 
 export class ImportAPI extends AuthRESTDataSource {
-  public baseURL = `${env?.api.fileSystemImporterServiceUrl}/`;
+  public baseURL = `${this.environment.api.fileSystemImporterServiceUrl}/`;
 
   async getDirectories(dir: string): Promise<Directory[]> {
     const directories = await this.get(`importer/directories?dir=${dir}`);
